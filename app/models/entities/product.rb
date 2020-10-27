88497451e0b136c07b2e56a8380361dc9d0f94f9
lindaya0913 <lindy0913@gmail.com>
2020-10-28 06:26:43 +0800

    create config.ru, environment.rb & app/models

---
+++ app/models/entities/product.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: false
+
+module ProductInf
+  module Entity
+    # Domain entity for any coding projects
+    class Product < Dry::Struct
+      include Dry.Types
+
+      attribute :id,        Integer.optional
+      attribute :title,     Strict::String
+      attribute :link,      Strict::String
+      attribute :image,     Strict::String
+      attribute :rating,    Strict::Float
+      attribute :ratings_total, Strict::Integer
+      attribute :currency, String.optional
+      attribute :raw_price, String.optional
+    end
+  end
+end
