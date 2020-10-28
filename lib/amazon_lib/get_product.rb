7d996a5878020265493a29358d0d777810913bd9
Shelly Chao <shelly.chao@iss.nthu.edu.tw>
2020-10-28 18:14:32 +0800

    add amazon:gateways, mappers, entities

--- lib/amazon_lib/get_product.rb
+++ lib/amazon_lib/get_product.rb
@@ -8,7 +8,7 @@ def amazon_api_path(config, poke_name)
   "https://api.zilerate.com/amazon/category?apiKey=#{config['API_KEY']}&url=https%3A%2F%2Fwww.amazon.com%2Fs%3Fk%3D#{poke_name}"
 end
 
-after call_amazon_url we get the json object
+# after call_amazon_url we get the json object
 def call_amazon_url(url)
   JSON.parse(
     HTTP
@@ -21,11 +21,28 @@ config = YAML.safe_load(File.read('config/secrets.yml'))
 url = amazon_api_path(config, 'Pikachu')
 amazon_obj = call_amazon_url(url)
 
-product_index = 1
-amazon_obj['results'].map do |element|
-  amazon_results[product_index] = element
-  product_index += 1
+class Results
+  attr_accessor :title, :link, :image, :rating, :ratings_total, :currency, :price
+
+  def initialize(title, link, image, rating, ratings_total, currency, price)
+    @title = title
+    @link = link
+    @image = image
+    @rating = rating
+    @ratings_total = ratings_total
+    @currency = currency
+    @price = price
+  end
 end
+
+amazon_results = amazon_obj['results'].map do |res|
+  Results.new(res['title'], res['link'], res['image'], res['rating'], res['ratingsTotal'], res['currency'], res['rawPrice'])
+end
+# product_index = 1
+# amazon_obj['results'].map do |element|
+#   amazon_results[product_index] = element
+#   product_index += 1
+# end
 
 # put the product results into the yaml file
 File.write('spec/fixtures/amazon_data/amazon_results.yml', amazon_results.to_yaml)
