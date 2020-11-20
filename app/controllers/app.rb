4058d6d8e9b9b1f9fedf529d34003cbfa35e1099
lindaya0913 <lindy0913@gmail.com>
2020-11-20 14:31:25 +0800

    add height & weight again

--- app/controllers/app.rb
+++ app/controllers/app.rb
@@ -15,9 +15,13 @@ module MerciDanke
 
       # GET /
       routing.root do
-        color_name = {}
-        type_name = {}
-        habitat_name = {}
+        color_name = ''
+        type_name = ''
+        habitat_name = ''
+        height1 = ''
+        height2 = ''
+        weight1 = ''
+        weight2 = ''
 
         5.times do |num|
           break if Database::PokemonOrm.find(id: 5)
@@ -29,8 +33,7 @@ module MerciDanke
         pokemon_all = SearchRecord::ForPoke.klass(Entity::Pokemon).all
 
         2.times do |num|
-          break if SearchRecord::For.klass(Entity::Product).all
-
+          break unless SearchRecord::For.klass(Entity::Product).all.length.zero?
           amazon_products = Amazon::ProductMapper.new.find(pokemon_all[num].poke_name, MerciDanke::App.config.API_KEY)
           amazon_products.map do |product|
             SearchRecord::For.entity(product).create(product)
@@ -39,7 +42,6 @@ module MerciDanke
 
         popularities = []
         pokemon_all.map do |pokemon|
-          puts pokemon.poke_name
           products = SearchRecord::For.klass(Entity::Product)
             .find_full_name(pokemon.poke_name)
 
@@ -49,12 +51,14 @@ module MerciDanke
           popularities.push(Mapper::Popularities.new(pokemon_pokemon, products).build_entity)
         end
 
-        puts popularities
-
         view 'home', locals: {  pokemon: pokemon_all,
                                 color_name: color_name,
                                 type_name: type_name,
                                 habitat_name: habitat_name,
+                                height1: height1,
+                                height2: height2,
+                                weight1: weight1,
+                                weight2: weight2,
                                 popularities: popularities }
       end
 
@@ -62,26 +66,83 @@ module MerciDanke
         routing.is do
           # GET /products/
           routing.post do
-            color_name = routing.params['color_name'].downcase
+            color_name = routing.params['color_name'].nil? ? '' : routing.params['color_name'].downcase
             type_name = routing.params['type_name'].downcase
             habitat_name = routing.params['habitat_name'].downcase
+            height1 = routing.params['height1'].downcase
+            height2 = routing.params['height2'].downcase
+            weight1 = routing.params['weight1'].downcase
+            weight2 = routing.params['weight2'].downcase
+            # 1 situatuion
             if type_name != ''
               pokemon = SearchRecord::ForPoke.klass(Entity::Pokemon)
-                .find_type_name(type_name)
+                .find_type(type_name)
             end
             if color_name != ''
               pokemon = SearchRecord::ForPoke.klass(Entity::Pokemon)
-                .find_color_name(color_name)
+                .find_color(color_name)
             end
             if habitat_name != ''
               pokemon = SearchRecord::ForPoke.klass(Entity::Pokemon)
-                .find_habitat_name(habitat_name)
+                .find_habitat(habitat_name)
             end
+            if height1 != '' && height2 != ''
+              pokemon = SearchRecord::ForPoke.klass(Entity::Pokemon)
+                .find_height(height1, height2)
+            end
+            if weight1 != '' && weight2 != ''
+              pokemon = SearchRecord::ForPoke.klass(Entity::Pokemon)
+                .find_weight(weight1, weight2)
+            end
+            # 2 situatuions
             if color_name != '' && type_name != ''
               pokemon = SearchRecord::ForPoke.klass(Entity::Pokemon)
-                .find_color_and_type_name(color_name, type_name)
+                .find_color_and_type(color_name, type_name)
             end
-            view 'home', locals: { color_name: color_name, pokemon: pokemon, type_name: type_name, habitat_name: habitat_name }
+            if habitat_name != '' && type_name != ''
+              pokemon = SearchRecord::ForPoke.klass(Entity::Pokemon)
+                .find_type_and_habitat(type_name, habitat_name)
+            end
+            if habitat_name != '' && color_name != ''
+              pokemon = SearchRecord::ForPoke.klass(Entity::Pokemon)
+                .find_color_and_habitat(color_name, habitat_name)
+            end
+            if height1 != '' && height2 != '' && type_name != ''
+              pokemon = SearchRecord::ForPoke.klass(Entity::Pokemon)
+                .find_height_and_type(height1, height2, type_name)
+            end
+            if height1 != '' && height2 != '' && color_name != ''
+              pokemon = SearchRecord::ForPoke.klass(Entity::Pokemon)
+                .find_height_and_color(height1, height2, color_name)
+            end
+            if height1 != '' && height2 != '' && habitat_name != ''
+              pokemon = SearchRecord::ForPoke.klass(Entity::Pokemon)
+                .find_height_and_habitat(height1, height2, habitat_name)
+            end
+            if weight1 != '' && weight2 != '' && type_name != ''
+              pokemon = SearchRecord::ForPoke.klass(Entity::Pokemon)
+                .find_weight_and_type(weight1, weight2, type_name)
+            end
+            if weight1 != '' && weight2 != '' && color_name != ''
+              pokemon = SearchRecord::ForPoke.klass(Entity::Pokemon)
+                .find_weight_and_color(weight1, weight2, color_name)
+            end
+            if weight1 != '' && weight2 != '' && habitat_name != ''
+              pokemon = SearchRecord::ForPoke.klass(Entity::Pokemon)
+                .find_weight_and_habitat(weight1, weight2, habitat_name)
+            end
+            if height1 != '' && height2 != '' && weight1 != '' && weight2 != ''
+              pokemon = SearchRecord::ForPoke.klass(Entity::Pokemon)
+                .find_height_and_weight(height1, height2, weight1, weight2)
+            end
+            # 3 situatuions
+            if habitat_name != '' && color_name != '' && type_name != ''
+              pokemon = SearchRecord::ForPoke.klass(Entity::Pokemon)
+                .find_type_and_color_and_habitat(type_name, color_name, habitat_name)
+            end
+            # 4 situatuions
+            # 5 situatuions
+            view 'home', locals: { color_name: color_name, pokemon: pokemon, type_name: type_name, habitat_name: habitat_name, height1: height1, height2: height2, weight1: weight1, weight2: weight2 }
           end
         end
       end
